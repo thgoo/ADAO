381da077259670e994d66fb9062c7994396d3a9e
thiaugpr <thiagoeu@gmail.com>
2011-05-17 09:49:51 -0300

    terceira
    Change-Id: Ib7d662cc555d121f79e461d818f7b94c34b07f96

--- src/main/java/br/com/adao/Adao.java
+++ src/main/java/br/com/adao/Adao.java
@@ -3,7 +3,7 @@ package br.com.adao;
 public class Adao {
 
 	public static void main(String[] args) {
-		System.out.println("2ª atualização, a primeira foi foda...");
+		System.out.println("3ª atualização.");
 
 	}
 
