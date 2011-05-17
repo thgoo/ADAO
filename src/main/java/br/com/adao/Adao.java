10e195cccef8063a4405909d3726e81a244ca96e
thiaugpr <thiagoeu@gmail.com>
2011-05-17 09:46:37 -0300

    2ª atualização
    Change-Id: I29f260022564565d3dce1e264b6702c960889d14

--- src/main/java/br/com/adao/Adao.java
+++ src/main/java/br/com/adao/Adao.java
@@ -3,7 +3,7 @@ package br.com.adao;
 public class Adao {
 
 	public static void main(String[] args) {
-		System.out.println("Mexi aqui e atualizei no github.com :D");
+		System.out.println("2ª atualização, a primeira foi foda...");
 
 	}
 
