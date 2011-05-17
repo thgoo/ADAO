1404bd9ff5d1d25bb2657750e2462aefdb5d1eb6
thiaugpr <thiagoeu@gmail.com>
2011-05-17 09:40:58 -0300

    Só pra testar...
    Change-Id: I32ff22e545432a93b03caadb683588090f44b715

--- src/main/java/br/com/adao/Adao.java
+++ src/main/java/br/com/adao/Adao.java
@@ -3,7 +3,7 @@ package br.com.adao;
 public class Adao {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Mexi aqui e atualizei no github.com :D");
 
 	}
 
